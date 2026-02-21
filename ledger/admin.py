9f98abd6b9c0f7ff9b818187a77f484681920188
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-02-21 16:10:52 +0800

    added an inline admin view for recipeingredients within recipe admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline]
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
