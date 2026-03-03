56e2aebb37ead6912d1c3240e8bb231be2821bad
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-03-03 15:48:12 +0800

    added user model, updated model, fixed migrations

--- ledger/models.py
+++ ledger/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from datetime import datetime
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField(blank=True)
 
 
 class Ingredient(models.Model):
@@ -17,6 +23,10 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="recipe_author")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}"
